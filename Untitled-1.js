e4107439a5b13865f913f004d764b4c2ac426509
happla <104832348+happla@users.noreply.github.com>
2022-10-22 13:11:20 +1100

    js.file of how old function

---
+++ Untitled-1.js
@@ -0,0 +1,26 @@
+// Write your function here:
+
+const howOld = (age, year) => {
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    
+    const yearDifference = year - currentYear
+    const newAge = age + yearDifference
+    
+    if (newAge > age) {
+      return `You will be ${newAge} in the year ${year}`
+    }
+    else if (newAge < 0) {
+      return `The year ${year} was ${-newAge} years before you were born`
+    }
+    else {
+     return `You were ${newAge} in the year ${year}`
+    }
+    }
+    
+    
+    
+    // Once your function is written, write function calls to test your code!
+    
+    console.log(howOld(25,1995))
+    
